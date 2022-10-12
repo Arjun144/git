25256ed1ea5999532a0618eea14d6ea71a233c55
Arjun144 <rachakondaarjunrao@gmail.com>
2022-10-12 15:35:50 +0530

    First commit

--- palindrome.bash
+++ palindrome.bash
@@ -1,3 +1,28 @@
+<<<<<<< HEAD
+input="file.txt"
+
+while IFS= read -r str
+do
+  
+  revstr=""
+  N=${#str}
+
+  for((i=${N-1}; i>=0; i--)) 
+  do
+   revstr=$revstr${str:$i:1}
+  done
+
+  if [[ "$str" == "$revstr" ]]
+  then
+    echo "${str} - is a palindrome."
+    
+  else
+    echo "${str} - is not a palindrome."
+    
+  fi
+  
+done < "$input"
+=======
 read -p "Enter string: " str
 
 N=${#str}
@@ -13,3 +38,4 @@ then
 else
     echo "It is not a palindrome."
 fi
+>>>>>>> d2d1717... text file commit
